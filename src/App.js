111aa60aebbd8bae66d36bd791f231707c982dae
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-22 16:34:43 -0400

    add color

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  useEffect(() => {
+    window.onmousemove = e => {
+      document.body.style.backgroundColor = `hsl(${e.clientX}, 60%, 60%)`
+    }
+  }, [])
+
+  return "";
 }
 
 export default App;
